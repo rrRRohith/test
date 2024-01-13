75ff3c5a6c575c4f858253fab75c647b7f51f4da
Bogachi <rohithr.indigital@gmail.com>
2024-01-13 12:01:54 +0530

    fix image upload

--- app/Services/Uploader.php
+++ app/Services/Uploader.php
@@ -5,8 +5,8 @@ use Illuminate\Http\Request;
 
 trait Uploader{
     public function upload($field, Request $request){
-        if ($request->hasFile('logo')) {
-            $image = $request->file('logo');
+        if ($request->hasFile($field)) {
+            $image = $request->file($field);
             $imageName = time() . '_' . $image->getClientOriginalName();
             $image->storeAs('public/images', $imageName);
             return $imageName;
